2bd6d6b4143f6ac2b398429568e82ec55e98a2cb
Lee Jaeheon <ti6419@gmail.com>
2023-12-07 17:31:01 +0900

    chore: 멀티모듈 프로젝트로 변경

---
+++ membership-service/src/main/java/com/violetbeach/membershipservice/MembershipServiceApplication.java
@@ -0,0 +1,13 @@
+package com.violetbeach.membershipservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MembershipServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MembershipServiceApplication.class, args);
+    }
+
+}
---
+++ membership-service/src/test/java/com/violetbeach/membershipservice/MembershipServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.violetbeach.membershipservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MembershipServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
