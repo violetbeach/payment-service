4ee69e496c5149ec89e5ff37b71505c6b92643f1
Lee Jaeheon <ti6419@gmail.com>
2023-12-07 22:54:04 +0900

    chore: SpringBoot 버전 변경 to 3.1.2 (Swagger 이슈 해결)

--- membership-service/src/main/java/com/violetbeach/membership/SwaggerConfig.java
+++ membership-service/src/main/java/com/violetbeach/membership/SwaggerConfig.java
@@ -11,7 +11,6 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
 
     @Bean
@@ -19,7 +18,7 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
             .apiInfo(apiInfo())
             .select()
-            .apis(RequestHandlerSelectors.basePackage("com.fastcampuspay.membership"))
+            .apis(RequestHandlerSelectors.basePackage("com.violetbeach.membership"))
             .paths(PathSelectors.any())
             .build();
     }
