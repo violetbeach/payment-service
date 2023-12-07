ee897b1046db76e88845878704e3a71941a0765a
Lee Jaeheon <ti6419@gmail.com>
2023-12-07 20:56:07 +0900

    feat: Hexagonal Architecture 컴포넌트 애노테이션 생성

---
+++ membership-service/src/main/java/common/UseCase.java
@@ -0,0 +1,20 @@
+package common;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.stereotype.Service;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Service
+public @interface UseCase {
+
+    @AliasFor(annotation = Service.class)
+    String value() default "";
+
+}
---
+++ membership-service/src/main/java/common/WebAdapter.java
@@ -0,0 +1,25 @@
+package common;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@RestController
+@RequestMapping
+public @interface WebAdapter {
+
+    @AliasFor(annotation = RestController.class)
+    String value() default "";
+
+    @AliasFor(annotation = RequestMapping.class, attribute = "path")
+    String path() default "";
+
+}
